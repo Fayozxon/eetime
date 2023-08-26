b10a08059df37cc518227d494139fe755af348c3
Fayozxon <83503559+Fayozxon@users.noreply.github.com>
2023-08-26 20:57:39 +0500

    added preview cards

--- src/components/ClockStyles.vue
+++ src/components/ClockStyles.vue
@@ -8,15 +8,26 @@
         <div class="container">
 
             <div class="previews-section__box">
-                <img src="../assets/img.png" alt="Preview Image">
-                <img src="../assets/img.png" alt="Preview Image">
-                <img src="../assets/img.png" alt="Preview Image">
-                <img src="../assets/img.png" alt="Preview Image">
-                <img src="../assets/img.png" alt="Preview Image">
-                <img src="../assets/img.png" alt="Preview Image">
-                <img src="../assets/img.png" alt="Preview Image">
-                <img src="../assets/img.png" alt="Preview Image">
-                <img src="../assets/img.png" alt="Preview Image">
+                <!-- card -->
+                <div v-for="n in 9" class="card">
+                    <img src="../previews/clock-1.png" class="preview" alt="Preview Image">
+                    <div class="card__overlay">
+                        <div class="card__overlay--info">
+                            <h3>Color palette</h3>
+                            <p>
+                                <span>Minimal</span>
+                                <span>Customizable</span>
+                            </p>
+                        </div>
+                        <button class="card__overlay--love">
+                            <img src="../assets/icon-heart.svg" alt="">
+                        </button>
+                        <button class="card__overlay--action">
+                            <img src="../assets/icon-arrow-up.svg" alt="">
+                        </button>
+                    </div>
+                </div>
+                <!-- /card -->
             </div>
             
             <a href="#header" class="btn">
@@ -38,8 +49,98 @@
         grid-template-columns: 1fr 1fr 1fr;
         gap: 20px;
 
-        img {
-            width: 100%;
+        .card {
+            position: relative;
+            background: red;
+            border-radius: 10px;
+            overflow: hidden;
+
+            .preview {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+            }
+            
+            &__overlay {
+                position: absolute;
+                top: 0;
+                left: 0;
+                right: 0;
+                bottom: 0;
+                background: linear-gradient(to bottom, rgba(0,0,0,0), #050608);
+                opacity: 0;
+                transition: all 200ms ease-in-out;
+
+                &--info {
+                    max-width: 80%;
+                    position: absolute;
+                    bottom: 30px;
+                    left: 30px;
+
+                    h3 {
+                        font-weight: 700;
+                    }
+
+                    p {
+                        display: flex;
+                        gap: 10px;
+                        flex-wrap: wrap;
+                        text-transform: uppercase;
+                        color: $clr-lt-blue;
+                        font-weight: 700;
+                        font-size: $fs-sm;
+                    }
+                }
+
+                &--love {
+                    position: absolute;
+                    top: 30px;
+                    right: 30px;
+                    padding: 5px 10px;
+                    padding-top: 10px;
+                    border-radius: 7px;
+                    border: 2px solid $clr-st-dk-blue;
+                    background: $clr-bg-dk-blue;
+                    cursor: pointer;
+
+                    img {
+                        width: 24px;
+                    }
+
+                    &.active {
+                        background: #CB0049;
+                        border-color: $clr-lt-blue;
+                    }
+                }
+
+                &--action {
+                    position: absolute;
+                    right: 30px;
+                    bottom: 30px;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    width: 70px;
+                    height: 70px;
+                    border-radius: 50%;
+                    border: none;
+                    background: $clr-white;
+                    cursor: pointer;
+                    
+                    &:hover img {
+                        transform: rotate(45deg);
+                    }
+                    
+                    img {
+                        transform: rotate(90deg);
+                        transition: all 200ms ease-in-out;
+                    }
+                }
+            }
+
+            &:hover .card__overlay {
+                opacity: 1;
+            }
         }
     }
 
